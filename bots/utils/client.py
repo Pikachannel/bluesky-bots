a3f69f2e48f8b2dbc38513b7cff43c482eb9ac5b
Ashley <wheeliedaniel@gmail.com>
2025-12-07 22:27:59 +0000

    Update client.py

--- bots/utils/client.py
+++ bots/utils/client.py
@@ -1,15 +1,16 @@
 # -------- Imports --------
-from atproto import Client, client_utils
+from atproto import Client
+from typing import Optional
 
 # -------- Login Function --------
-async def login(username, password):
+async def login(username: str, password: str) -> Optional[Client]:
     try:
         # -- Setup client connection
         client = Client()
         profile = client.login(username, password)
         client.profile = profile
         print(f"[Client] Logged in as {profile.handle} ({profile.did})")
-        return client, profile # Return both the client and profile objects
+        return client # Return both the client and profile objects
     except Exception as e:
         print(f"[Client] An error occured while creating a client session, {e}")
-        return False, False # Return false if the login failed
+        return None # Return none if the login failed
