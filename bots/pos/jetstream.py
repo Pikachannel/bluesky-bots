2f6cd82c9dea50391bfdb6e55c812667d14646d5
Ashley <wheeliedaniel@gmail.com>
2025-12-07 22:26:50 +0000

    Added a disconnect function

--- bots/pos/jetstream.py
+++ bots/pos/jetstream.py
@@ -7,13 +7,14 @@ from websockets.exceptions import ConnectionClosedError
 # -------- Websocket Class --------
 class Websocket():
     # -- Setup websocket variables
-    def __init__(self, endpoint: str = None):
+    def __init__(self):
         self.endpoint = "wss://jetstream1.us-east.bsky.network/subscribe"
         self.reconnect = 1
         self.connected = False
+        self.ws = None
 
     # -- Connect function
-    async def connect(self, queue):
+    async def connect(self, queue: asyncio.Queue) -> None:
         # Try to connect infinitely 
         self.connected = True
         while self.connected:
@@ -21,6 +22,8 @@ class Websocket():
                 # Connect to the bsky jetstream
                 async with websockets.connect(self.endpoint) as ws:
                     print(f"[Websocket] Connected to {self.endpoint}")
+                    self.ws = ws
+
                     async for raw_message in ws: # Get new messages
                         try:
                             message = json.loads(raw_message)
@@ -35,4 +38,10 @@ class Websocket():
             except Exception as e:
                 print(f"[Websocket] An error has occured, {e} reconnecting shortly...")
                 await asyncio.sleep(self.reconnect) # Wait to reconnect upon error
-
+    
+    # -- Disconnect function
+    async def disconnect(self) -> None:
+        self.connected = False
+        if self.ws:
+            await self.ws.close() # Disconnect if connection is open
+            print("[Websocket] Disconnected.")
